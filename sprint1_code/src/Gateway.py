43a82f642047518505f7de4ad4c62dff15ae023e
EvilClam <shaunschreiber@gmail.com>
2013-06-18 16:43:16 +0200

    Created Gateway

---
+++ sprint1_code/src/Gateway.py
@@ -0,0 +1,20 @@
+'''
+Created on 18 Jun 2013
+
+@author: evilclam
+'''
+
+class Gateway(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+    def JSON_to_GeoJSON(self):
+        '''
+        
+        ''' 
