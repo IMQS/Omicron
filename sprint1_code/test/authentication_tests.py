3a8bace88b91223a81fb3dea73216d7fe97ec612
EvilClam <shaunschreiber@gmail.com>
2013-06-18 12:00:32 +0200

    Shaun added his test case.

--- sprint1_code/test/authentication_tests.py
+++ sprint1_code/test/authentication_tests.py
@@ -28,3 +28,6 @@ class StandAloneTests(TestCase):
         """Test the login function when an error happens."""
         mock_open.side_effect = IOError()
         self.assertFalse(auth.login('george', 'bosco'))
+        
+    def test_mytest(self):
+        self.assert_(True, "Passes basic assert")
