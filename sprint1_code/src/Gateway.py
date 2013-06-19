aef075435f6bf7d9e6bd29b3241eaa52caeac656
EvilClam <shaunschreiber@gmail.com>
2013-06-19 12:59:36 +0200

    Basic format of gateway is done.

--- sprint1_code/src/Gateway.py
+++ sprint1_code/src/Gateway.py
@@ -3,20 +3,35 @@ Created on 18 Jun 2013
 
 @author: Shaun Schreiber
 '''
-import social_platform
-class Gateway(object):
+from social_platform import social_platform as sp
+
+class gateway(object):
     '''
         Extract raw data from the sosial media API's and returns a GeoJSON object. 
     '''
          
-    def select_sosial_media(self, names):
+    def _select_sosial_media(self, names):
         '''
             Checks to see if the selected social API's are available. If not then the following exception 
             will be raised "NotImplementedError" else it will return a list of social_platform objects.
+            
+            @param self: a Pointer to the current object.
+            @type self: gateway
             @param names: list of string names.
             @type names: List
             @return: List of social_platform objects. 
             @rtype: social_platform
         '''
         
-    
+    def execute_requests(self, platforms, key_words):
+        '''
+            @param self: a Pointer to the current object.
+            @type self: gateway
+            @param platforms: List of social media platforms.
+            @type platforms: List
+            @param key_words: List of key words that will be used in the search.
+            @type key_words: List
+            @return: The raw data that was fetched from the social media API's.
+            @rtype: GeoJSON Array
+        '''
+        
