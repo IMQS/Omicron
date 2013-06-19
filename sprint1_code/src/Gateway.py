32226e3386d557dc269082ed03e463a29f86b225
EvilClam <shaunschreiber@gmail.com>
2013-06-19 11:50:17 +0200

    gateway and socail_platform frame almost complete

--- sprint1_code/src/Gateway.py
+++ sprint1_code/src/Gateway.py
@@ -1,20 +1,22 @@
 '''
 Created on 18 Jun 2013
 
-@author: evilclam
+@author: Shaun Schreiber
 '''
-
+import social_platform
 class Gateway(object):
     '''
-    classdocs
+        Extract raw data from the sosial media API's and returns a GeoJSON object. 
     '''
-
-
-    def __init__(self):
+         
+    def select_sosial_media(self, names):
         '''
-        Constructor
-        '''
-    def JSON_to_GeoJSON(self):
+            Checks to see if the selected social API's are available. If not then the following exception 
+            will be raised "NotImplementedError" else it will return a list of social_platform objects.
+            @param names: list of string names.
+            @type names: List
+            @return: List of social_platform objects. 
+            @rtype: social_platform
         '''
         
-        ''' 
+    def
---
+++ sprint1_code/src/social_platform.py
@@ -0,0 +1,39 @@
+'''
+Created on 19 Jun 2013
+
+@author: evilclam
+'''
+
+class social_platform(object):
+    '''
+    
+    ''' 
+    def request_geographical(self, criteria = None, center = None, radius = None):
+        '''
+            Queries the underlining social API with the search area defined by a circle. If any of the parameter are none then the query gets rejected 
+            and the following error message will be returned "query not suitable".
+            
+            @param self: Pointer to the current object.
+            @type self: social_platform  
+            @param criteria: a List of key words.
+            @type criteria: List 
+            @param center: It is a tuple that consists of longitude and latitude.
+            @type center: Tuple of floats
+            @param radius: Is the distance from the center that will be covered by the search.
+            @type radius: float 
+        '''
+        raise NotImplementedError
+    
+    def repuest_area(self, criteria = None, area = None ):
+        '''
+            Queries the underlining social API with the search area defined by a geographical area e.g. Cape Town. If any of the parameter are none then the query gets rejected 
+            and the following error message will be returned "query not suitable".
+            
+            @param self: Pointer to the current object.
+            @type self: social_platform  
+            @param criteria: a List of key words.
+            @type criteria: List 
+            @param area: Name of the area. Will be in the following format (country state city)
+            @type area: String 
+        '''
+        raise NotImplementedError
