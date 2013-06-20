2c21c8e8a6e53cd9ce875d2806940328fc674c06
Leeward <michelle.arzul@gmail.com>
2013-06-20 12:22:53 +0200

    Added png exporting, modularised, changed variance function and further documented heatmap.

--- sprint1_code/src/map_plot.py
+++ sprint1_code/src/map_plot.py
@@ -26,6 +26,7 @@ def random_coords(num, bounds):
 	'''Returns random points.
 	@param num The number of points to generate.
 	@param bounds The bounding coordinates of the points to generate, in the form [left, right, bottom, top].
+	@return A list of 2-tuples of floats that represents a set of X, Y coordinates.
 	'''
 	from random import random as r
 	points = []
@@ -37,33 +38,27 @@ def random_coords(num, bounds):
 		points.append((x, y))
 	return points
 
-def heatmap(n, bounds, coords):
-	'''Creates a heatmap for the given coordinates or random points. Variance is adjusted according to point density..
-	@param n Number of points. Only matters when random points are being calculated since it is recalculated if coordinates are provided.
+def heatmap(bounds, coords):
+	'''Creates a heatmap within the specified bounds for the given coordinates. Variance is adjusted according to point density.
 	@param bounds A list of 4 floats that denote the left, right, bottom, and top limits of the extent respectively.
-	@param coords List of 2-tuples of floats that represent point coordinates. If None, n random points are generated within bounds.
+	@param coords List of 2-tuples of floats that represent point coordinates.
+	@return A list of lists of floats the represents the pixel matrix of the heatmap.
 	'''
 	import numpy as np
-	from scipy import misc
-	import matplotlib.pyplot as plt
 	
-	if coords == None:
-		coords = random_coords(n, bounds)
-		print n, "random points generated."
-	else:
-		n = len(coords)
-		print n, "points provided."
+	n = len(coords)
+	print n, "points provided."
 	
 	rdim = bounds[1] - bounds[0]
 	cdim = bounds[3] - bounds[2]
 
 	heatmap = np.array([[0.]*rdim]*cdim)
 	
-	print "For bounds "+str(bounds)+", extent is "+str(rdim)+" by "+str(cdim)+"."
+	print "For bounds "+str(bounds)+", extent is "+str(rdim)+" by "+str(cdim)+" and area is "+str(rdim*cdim)+"."
 	
 	p_dens = float(n)/(rdim*cdim)
-	var = 1 / (32*p_dens)
-	print "Point density of "+str(p_dens)+" gives a variance of "+str(var)+"."
+	var = 1 / (8*p_dens)
+	print "Point density of "+str(p_dens)+" gives a variance estimate of "+str(var)+"."
 	
 	for point in coords:
 		print "Processing:",str(point)
@@ -72,34 +67,54 @@ def heatmap(n, bounds, coords):
 				heatmap[i][j] += gauss(var,
 				heat_dist(point,j+bounds[0]+.5,i+bounds[2]+.5))
 				# the +.5 is to account for pixel center
-		
+	
+	return heatmap
+
+def show_heatmap(heatmap, bounds):
+	'''Displays a heatmap within certain bounds.
+	@param heatmap The list of lists of floats that represents the pixel matrix of the heatmap.
+	@param bounds The bounding box of the figure.
+	'''
+	import numpy as np
+	import matplotlib.pyplot as plt
+	
+	print "Displaying heat map..."
 	plt.xlim(bounds[0], bounds[1])
 	plt.ylim(bounds[2], bounds[3])
-	
 	plt.imshow(heatmap, cmap='hot', origin='lower', extent=bounds)
+	plt.show()
+	print "Done."
 
-	x_coords = []
-	y_coords = []
-	for point in coords:
-		x_coords.append(point[0])
-		y_coords.append(point[1])
-	plt.scatter(x_coords, y_coords)
+def save_heatmap(heatmap):
+	''' Saves a heatmap to file, in a subfolder named "heatmaps" with the naming convention "heatmap_###" where ### is a 3-digit number that is 1 larger than the largest number already present.
+	@param heatmap The list of lists of floats that represents the pixel matrix of the heatmap.
+	'''
+	import numpy as np
+	from scipy import misc
+	from os import listdir
 	
 	print "Saving to disk..."
 	rescale = heatmap * 255. / heatmap.max()
 	rescale = rescale[::-1]
-	misc.imsave('test.png', rescale)
+	maxi = 0
+	flist = listdir('./heatmaps')
+	for item in flist:
+		if item.startswith('heatmap_'):
+			num = int(item.__getslice__(8, len(item)-4))
+			if num > maxi:
+				maxi = num
+	num = str(maxi+1)
+	while len(num) < 3:
+		num = '0'+num
+	misc.imsave('heatmaps/heatmap_'+num+'.png', rescale)
 	print "Done."
 
-	print "Displaying heat map..."
-	plt.show()
-	print "Done."
-	
 def heat_dist(point, x, y):
 	'''Returns the Euclidean distance between a point and a pixel on an image.
 	@param point The 2-tuple of the point's coordinates.
 	@param x The x-coordinate of the pixel.
 	@param y The y-coordinate of the pixel.
+	@return The Euclidean distance between the point and the pixel.
 	'''
 	from math import sqrt
 	return sqrt((point[0]-x)**2+(point[1]-y)**2)
@@ -108,6 +123,7 @@ def gauss(var, x):
 	'''Returns the y value at x for a normal distribution with variance var in the form f(x) = e^(-(x^2)/(2s^2)) where s is the spread. Note: the center of the curve is 0 since no mean is defined, and the maximum value is always 1.
 	@param var The variance to use for this curve.
 	@param x The distance from the point.
+	@return The value of the given curve at x.
 	'''
 	from math import e
 	return e ** (-(x*x)/(2*var))
@@ -126,9 +142,11 @@ def plot_gauss(var, lim):
 
 if __name__ == "__main__":
 #	plot_random(20, 100)
-#	heatmap([(0.25,0.25),(0.5,0.5),(0.75,0.75)])
 #	plot_gauss(1, 3)
 #	coords = [[20,50],[25,65],[30,75],[35,90],[50,50],[65,10],[80,50]]
-	bounds = [-150,50,-50,100]
-	heatmap(10, bounds, None)
+	bounds = [-100,100,-100,100]
+	coords = random_coords(10, bounds)
+	heatmap = heatmap(bounds, coords)
+	show_heatmap(heatmap, bounds)
+	save_heatmap(heatmap)
 
