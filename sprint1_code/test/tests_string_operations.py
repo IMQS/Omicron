bd813fd0b07414b63ccc2b9e1e9257ff92351c99
Javonne <Javonne.Martin@gmail.com>
2013-06-20 10:04:00 +0200

    Updated tests_string_operations.py

--- sprint1_code/test/tests_string_operations.py
+++ sprint1_code/test/tests_string_operations.py
@@ -8,17 +8,17 @@ class test_class_string_operations(unittest.TestCase):
 	def setUp(self):
 		pass
 	''' Tests the concatination of "hel" and "lo" to produce "hello"'''
-	def test_string_concationation_hel_lo(self):
-		self.assertEqual(so.string_concationation("hel","lo"),"hello")
+	def test_string_concatination_hel_lo(self):
+		self.assertEqual(so.string_concatination("hel","lo"),"hello")
 	'''Tests the concatination of 4 and "lo" the method doesn't allow this and returns None'''
-	def test_string_concationation_4_lo(self):
-		self.assertEqual(so.string_concationation(4,"lo"),None)
+	def test_string_concatination_4_lo(self):
+		self.assertEqual(so.string_concatination(4,"lo"),None)
 	'''Tests the concatination of 4 and "hello" the method doesn't allow this and returns None'''
-	def test_string_int_concationation_4_hello(self):
-		self.assertEqual(so.string_int_concationation(4,"hello"), None)
+	def test_string_int_concatination_4_hello(self):
+		self.assertEqual(so.string_int_concatination(4,"hello"), None)
 	'''Tests the concatination of "hello" and 4 should return "hello4"'''
-	def test_string_int_concationation_hello_4(self):
-		self.assertEqual(so.string_int_concationation("hello",4), "hello4")
+	def test_string_int_concatination_hello_4(self):
+		self.assertEqual(so.string_int_concatination("hello",4), "hello4")
 if __name__ == "__main__":
 	unittest_main()
 		
