731c5480502a41797c4e6b579a816c1e07243e1c
EvilClam <shaunschreiber@gmail.com>
2013-06-24 13:30:47 +0200

    Adding changes to webservice and add apache_webpy documentation

--- sprint1_code/src/webservice.py
+++ sprint1_code/src/webservice.py
@@ -1,14 +1,14 @@
+#!/bin/bash python
 '''
 Created on 19 Jun 2013
 
 @author: evilclam
 '''
+import sys, os
+sys.path.append('/var/www/webpy-app/')
 import web
 import json
 import Gateway
-
 urls = ("/gateway","Gateway.gateway")
-
-if __name__ == '__main__':
-    app = web.application(urls, globals())
-    app.run()
+app = web.application(urls, globals())
+application = app.wsgifunc()
