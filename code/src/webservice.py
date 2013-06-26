bc9afd451b99d0dbbaacec401dc60cb076cc157a
EvilClam <shaunschreiber@gmail.com>
2013-06-26 11:52:34 +0200

    Working on webservice.

--- code/src/webservice.py
+++ code/src/webservice.py
@@ -8,8 +8,32 @@ import sys, os
 sys.path.append('/var/www/webpy-app/')
 import web
 import json
-import Gateway
-urls = ("/gateway","Gateway.gateway")
+import gateway
+
+class request_handler(object):
+    def GET(self):
+        '''
+            Handles GET requests received from users.
+            
+            @param self:
+            @type self: request_handler 
+            @TODO: Figure out how the parameters is going to be handled.
+        '''
+         
+    def POST(self):
+        '''
+            Handles POST requests received from users.
+            
+            @param self: a Instance of the current object.
+            @type self: request_handler
+            @param query: It holds the relevant data needed to perform the request. 
+            @rtype query: JSON Object with the following fields; platforms, preset, tags, function, location.
+            @TODO: figure out how the parameters are going to be handled.
+        '''
+        
+        
+urls = ("/request_handler","request_handler")
+
 app = web.application(urls, globals())
 application = app.wsgifunc()
 if __name__ == "__main__":
