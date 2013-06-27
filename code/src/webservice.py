6bedc3ebe8e233ad70a33f29ad3f38c64b5a1f39
EvilClam <shaunschreiber@gmail.com>
2013-06-27 10:01:41 +0200

    Added exception handling to webservice.

--- code/src/webservice.py
+++ code/src/webservice.py
@@ -8,12 +8,15 @@ import sys, os
 sys.path.append('/var/www/webpy-app/')
 import web
 import json
+import map_plot as mp
 from gateway import gateway
 
 class request_handler(object):
     '''
         Handles all incoming GET and POST requests
     '''
+    OK = {'success':'True'}
+    ERROR = {'succes':'False'}
     def GET(self):
         '''
             Handles GET requests received from users.
@@ -31,66 +34,89 @@ class request_handler(object):
             @param location: If location_type has the value "area" then location must be in the following format "country state city" else \
             if location_type has the value "radius" then location must be in the following format "longitude latitude <radius>km" 
             @type location: String
-            @todo: Figure out what to return.
+            @return: This will vary between request depending on what processing functions are applied to the social data that was mined.
+            @rtype: JSON Object
             
         '''
-        data = web.input()
-        platforms = data["platforms"].lstrip('u').split("#")
-        tags = data["tags"].lstrip('u').split("_")
-        function = data["function"]
+        return_data_and_status = {}
+        raw_data = web.input()
+        platforms = raw_data["platforms"].lstrip('u').split("#")
+        tags = raw_data["tags"].lstrip('u').split("_")
+        function = raw_data["function"]
         # For future use
-        location_type = data["location_type"]
-        location = data["location"].lstrip('u').split("#")
+        location_type = raw_data["location_type"]
+        location = raw_data["location"].lstrip('u').split("#")
         gatewayO = gateway()
         if (function == "heat_map"):
-            import map_plot as mp
             query_data = gatewayO.execute_requests(platforms, tags, (float(location[0]),float(location[1])),float(location[2]),['location'])
             total_coords = []
             for platform in platforms:
                 for coords in query_data[platform]:
                     total_coords = total_coords + coords
-            heat_map = {}
-            #@TODO: FIX the bounding box big problem
-            heat_map['Heat_map'] = mp.heatmap([float(location[0])-5, float(location[0])+5, float(location[1])-5, float(location[1])+5], total_coords)
-            return heat_map
+            try:
+                #@TODO: FIX the bounding box big problem
+                heat_map = mp.heatmap([float(location[0])-5, float(location[0])+5, float(location[1])-5, float(location[1])+5], total_coords)
+                mp.save_heatmap(heat_map)
+                return_data_and_status = self.OK
+            except:
+                msg = "The heatmap could not be generated or stored"
+                return_data_and_status = self.ERROR
+                return_data_and_status["message"] = msg
         else :
-            return "The function that was specified was not found."
-        return query_data
+            msg = "The function that was specified was not found."
+            return_data_and_status = self.ERROR
+            return_data_and_status["message"] = msg
+        return return_data_and_status
     
     def POST(self):
         '''
             Handles POST requests received from users.
             
-            @param self: a Instance of the current object.
+            @param self: Pointer to the current request_handler instance.
             @type self: request_handler
-            @param query: It holds the relevant data needed to perform the request. It has the following fields; platforms, preset, tags, function, location_type and location.
-            @rtype query: JSON Object
-            @todo: Figure out what to return.
+            @param platforms: List of hash(#) separated values that indicate which social media to use. 
+            @type platforms: String 
+            @param tags: List of hash(#) separated values that is used for the search criteria.
+            @type tags: String
+            @param function: Which function to apply after the data has been collected.
+            @type function: String
+            @param location_type: Either area or radius.
+            @type location_type: String
+            @param location: If location_type has the value "area" then location must be in the following format "country state city" else \
+            if location_type has the value "radius" then location must be in the following format "longitude latitude <radius>km" 
+            @type location: String
+            @return: This will vary between request depending on what processing functions are applied to the social data that was mined.
+            @rtype: JSON Object
         '''
-        data = web.input()
-        platforms = data["platforms"].lstrip('u').split("#")
-        tags = data["tags"].lstrip('u').split("_")
-        function = data["function"]
+        return_data_and_status = {}
+        raw_data = web.input()
+        platforms = raw_data["platforms"].lstrip('u').split("#")
+        tags = raw_data["tags"].lstrip('u').split("_")
+        function = raw_data["function"]
         # For future use
-        location_type = data["location_type"]
-        location = data["location"].lstrip('u').split("#")
+        location_type = raw_data["location_type"]
+        location = raw_data["location"].lstrip('u').split("#")
         gatewayO = gateway()
         if (function == "heat_map"):
-            import map_plot as mp
             query_data = gatewayO.execute_requests(platforms, tags, (float(location[0]),float(location[1])),float(location[2]),['location'])
             total_coords = []
             for platform in platforms:
                 for coords in query_data[platform]:
-                    print coords
-                    print total_coords
                     total_coords = total_coords + coords
-            heat_map = {}
-            #@TODO: FIX the bounding box big problem
-            heat_map['Heat_map'] = mp.heatmap([float(location[0])-5, float(location[0])+5, float(location[1])-5, float(location[1])+5], total_coords)
-            return heat_map
+            try:
+                #@TODO: FIX the bounding box big problem
+                heat_map = mp.heatmap([float(location[0])-5, float(location[0])+5, float(location[1])-5, float(location[1])+5], total_coords)
+                mp.save_heatmap(heat_map)
+                return_data_and_status = self.OK
+            except:
+                msg = "The heatmap could not be generated or stored"
+                return_data_and_status = self.ERROR
+                return_data_and_status["message"] = msg
         else :
-            return "The function that was specified was not found."
-        return query_data
+            msg = "The function that was specified was not found."
+            return_data_and_status = self.ERROR
+            return_data_and_status["message"] = msg
+        return return_data_and_status
 
 #:Groups the URL's and their corresponding actions.
 urls = ("/request_handler","request_handler")
