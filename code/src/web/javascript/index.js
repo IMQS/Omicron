9cadf5468cb39450c5a7a59ed6f85be0e8f1deba
Javonne Martin <Javonne.Martin@gmail.com>
2013-07-03 10:49:12 +0200

    website Html and Javascript

--- code/src/web/javascript/index.js
+++ code/src/web/javascript/index.js
@@ -1,3 +1,22 @@
 /*	don't actually need js for index
-var access_code
+	storage of authentication code for this session only !!
 */
+function store_codes(){
+	if(typeof(Storage)!=="undefined")
+	  {
+	  if (!sessionStorage.twitter_authentication_code)
+	    {
+	    sessionStorage.twitter_authentication_code="This is an authentication code"
+	    }
+	  else
+	    {
+		  
+	    }
+	  document.getElementById("result").innerHTML="You have clicked the button " + sessionStorage.clickcount + " time(s) in this session.";
+	  }
+	else
+	  {
+	  document.getElementById("result").innerHTML="Sorry, your browser does not support web storage...";
+	  }
+	}
+}
