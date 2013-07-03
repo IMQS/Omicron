330d4ad1cabdf1769e77a7727b75f126d0f3d81a
Javonne Martin <Javonne.Martin@gmail.com>
2013-07-03 13:37:48 +0200

    Rest Call in javascript added

--- code/src/web/javascript/index.js
+++ code/src/web/javascript/index.js
@@ -1,22 +1,41 @@
 /*	don't actually need js for index
 	storage of authentication code for this session only !!
-*/
-function store_codes(){
-	if(typeof(Storage)!=="undefined")
-	  {
-	  if (!sessionStorage.twitter_authentication_code)
-	    {
-	    sessionStorage.twitter_authentication_code="This is an authentication code"
-	    }
-	  else
-	    {
-		  
-	    }
-	  document.getElementById("result").innerHTML="You have clicked the button " + sessionStorage.clickcount + " time(s) in this session.";
-	  }
+ */
+function httprequest() {
+	var url = "http://host/path/to/resource";
+	var representationOfDesiredState = "The cheese is old and moldy, where is the bathroom?";
+	
+	var client = new XMLHttpRequest();
+
+	client.open("POST", url, false);
+
+	client.setRequestHeader("Content-Type", "text/plain");
+
+	client.send(representationOfDesiredState);
+
+	if (client.status == 200)
+		alert("The request succeeded!\n\nThe response representation was:\n\n"
+				+ client.responseText)
 	else
-	  {
-	  document.getElementById("result").innerHTML="Sorry, your browser does not support web storage...";
-	  }
+		alert("The request did not succeed!\n\nThe response status was: "
+				+ client.status + " " + client.statusText + ".");
+}
+
+function authenticate() {
+
+	return "Access_Token"
+}
+function store_codes() {
+	document.getElementById("result").innerHTML = "Sorry.";
+	if (typeof (Storage) !== "undefined") {
+		if (!sessionStorage.twitter_authentication_code) {
+			// sessionStorage.twitter_authentication_code=authenticate();
+			sessionStorage.twitter_authentication_code = "Authentication code present in storage";
+			document.getElementById("result").innerHTML = "New Auth code stored";
+		} else {
+			document.getElementById("result").innerHTML = sessionStorage.twitter_authentication_code;
+		}
+	} else {
+		document.getElementById("result").innerHTML = "Cant store things";
 	}
 }
