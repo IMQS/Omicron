2d48c79aff72ed64d20efb4957db81fcc47a9235
Javonne Martin <Javonne.Martin@gmail.com>
2013-07-04 09:56:08 +0200

    Apache Test for JS

--- code/src/web/javascript/leafletembed.js
+++ code/src/web/javascript/leafletembed.js
@@ -4,8 +4,16 @@
 function initmap() {
 	// set up the map
 	var map = L.map('mapHolder').setView([51.505, -0.09], 0);
+	
 	L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		/*'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'*/
 	    maxZoom: 18
 	}).addTo(map);
+	var heatmap = L.tileLayer('http://superfluous.imqs.co.za/omicron/request_handler?function=heat_map&platforms=twitter&tags=%23food&location_type=radius&location=33_50_50000&directory=/{z}/{x}/{y}', {
+		/*'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'*/
+	    maxZoom: 18
+	});
+	L.control.layers(null,{"Heat map":heatmap}).addTo(map);
+	
+
 }
