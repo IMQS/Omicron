a8cd93972ff72c8721fd85041caa29230be05e3b
Javonne Martin <Javonne.Martin@gmail.com>
2013-07-08 08:56:09 +0200

    Added heat map layer to leaflet

--- code/src/web/javascript/leafletembed.js
+++ code/src/web/javascript/leafletembed.js
@@ -5,15 +5,14 @@ function initmap() {
 	// set up the map
 	var map = L.map('mapHolder').setView([51.505, -0.09], 0);
 	
-	L.tileLayer('http://superfluous.imqs.co.za/omicron/request_handler?function=heat_map&platforms=twitter&tags=%23fire&location_type=radius&location=33_50_500&directory=/{z}/{x}/{y}.png', {
+	L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		/*'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'*/
 	    maxZoom: 18
 	}).addTo(map);
-/*	var heatmap = L.tileLayer('http://superfluous.imqs.co.za/omicron/request_handler?function=heat_map&platforms=twitter&tags=%23food&location_type=radius&location=33_50_50000&directory=/{z}/{x}/{y}', {
+	var heatmap = L.tileLayer('http://superfluous.imqs.co.za/omicron/request_handler?function=heat_map&platforms=twitter&tags=%23food&location_type=radius&location=33_50_50000&directory=/{z}/{x}/{y}', {
 		
 	    maxZoom: 18
 	});
-	L.control.layers(null,{"Heat map":heatmap}).addTo(map);
-*/	
+	L.control.layers(null,{"Heat map":heatmap}).addTo(map);	
 
 }
