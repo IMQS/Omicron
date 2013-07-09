416a2386185f515613f83090b824918bf4ee9a33
Javonne Martin <jovanne.martin@imqs.co.za>
2013-07-09 12:14:57 +0200

    index.js formats the data correctly for the webservice

--- code/src/web/javascript/index.js
+++ code/src/web/javascript/index.js
@@ -1,19 +1,145 @@
 /*	 
 	storage of authentication code for this session only !!
  */
-function OnSubmit(){
-	var search = document.findElementById("tags_list");
-	alert(search.value);
-	var lat= document.findElementById();
-	alert(lat.value);
-	var long= document.findElementById();
-	alert(long.value);
-	var Function= document.findElementById();
-	alert(Function.value);
-	var platform= document.findElementById();
-	alert(platform.value);
-	var locationtype= document.findElementById();
-	alert(locationtype.value);
+/**
+ * This checks the fields have been filled in before filling in the hidden fields with the data to format it correctly for the REST call
+ * 
+ */
+function OnSubmit() {
+	//parameters to submit
+	var tags = document.getElementsByName("tags")[0];
+	var platforms = document.getElementsByName("platforms")[0];
+	var location = document.getElementsByName("location")[0];
+	var location_type = document.getElementsByName("location_type")[0];
+	var func = document.getElementsByName("function")[0];
+	
+	//local parameters
+	var locationtype = document.getElementById("location_type_details");
+	var search = document.getElementById("tags");
+	var long = document.getElementById("longitude");
+	var lat = document.getElementById("latitude");
+	var area_radius = document.getElementById("radius_area");
+	
+
+	//temp storage
+	var parameters = "";
+	
+	console.log("OnSubmit");
+	
+	console.log("search element");
+	console.log(search.value);
+	//separates the search string by underscores for each component
+	if (search.value == "" || search.value == null){
+		return false;
+	} else {
+		var array = search.value.split(" ");
+		console.log(array.length);
+		parameters=parameters+array[0];
+		for(var i = 1;i< array.length;i++){
+			parameters = parameters +"_"+array[i];
+			
+		}
+		tags.value = parameters;			//set it back into the hidden input
+	}
+	
+	
+	parameters = "";
+	//sets the location type and area or GPS coordinates
+	console.log("locationtype element");
+	console.log(locationtype.value);
+	if(locationtype.value =="Area"){
+		location_type.value = "area"
+		if (area_radius.value == "" || area_radius.value == null){
+			console.log("area not specified also not implemented");
+			return false;
+		}else {
+			location.value = area_radius.value
+		}
+		
+	} else if(locationtype.value == "Radius") {
+		
+		location_type.value = "radius"; 
+
+
+		console.log("long element");
+		console.log(long.value);
+		if (long.value == "" || long.value == null)
+			return false;
+
+		console.log("lat element");
+		console.log(lat.value);
+		if (lat.value == "" || lat.value == null)
+			return false;
+		
+
+		console.log("radius element");
+		console.log(area_radius.value);
+		if (area_radius.value == "" || area_radius.value == null)
+			return false;
+		location.value = long.value+"_"+lat.value+"_"+area_radius.value
+	}
+	
+	parameters = "";
+	//Sets the function heat map or point map to the hidden func variable (from visible Function variable)
+	var Function = document.getElementById("function");
+	console.log("Function element");
+	console.log(Function.value);
+	if (Function.value == "" || Function.value == null)
+		return false;
+	if(Function.value == "Heat Map"){
+		func.value = "heat_map";
+	} else if(Function.value == "Point Map") {
+		func.value="point_map";
+	}
+	
+	//concatinates the strings separated by underscores of the platform names
+	var platform = document.getElementsByName("platform");
+	console.log("platform element");
+	console.log(platform);
+	var checkedplatform = null;
+	var boolean = false;
+	for ( var i = 0; i < platform.length; i++) {
+		if (platform[i].checked) {
+			if (checkedplatform == null) {
+				if (platform[i].value == "instagram") {
+					checkedplatform = platform[i].value;
+					boolean = true;
+					platform[i].disabled = true;
+					console.log("disabled instagram");
+				} else if (platform[i].value == "twitter") {
+					checkedplatform = platform[i].value;
+					boolean = true;
+					console.log("disabled twitter");
+					platform[i].disabled = true;
+				}
+			} else {
+				if (platform[i].value == "instagram") {
+					checkedplatform = platform[i].value + "_" + checkedplatform;
+					platform[i].disabled = true;
+				} else if (platform[i].value == "twitter") {
+					checkedplatform = platform[i].value + "_" + checkedplatform;
+					platform[i].disabled = true;
+				}
+			}
+		}
+	}
+	if(boolean == false){
+		console.log("No platform selected")
+		return false;
+	}
+	if (checkedplatform == null) {
+		console.log("No platform checked");
+		return false;
+	}
+
+	
+	
+	
+	platforms.value = checkedplatform;
+/*	var form = document.getElementById("form");
+	form.action = "http://superfluous.imqs.co.za/omicron/main.html"+parameters;
+	console.log(form);
+*/
 	return true;
 }
 /**
@@ -56,7 +182,8 @@ function store_codes(twitter_access_token, override) {
 		} else if (!sessionStorage.twitter_authentication_code) {
 			// sessionStorage.twitter_authentication_code=authenticate();
 			sessionStorage.twitter_authentication_code = twitter_access_token;
-			document.getElementById("result").innerHTML = "New Auth code stored" + twitter_access_token;
+			document.getElementById("result").innerHTML = "New Auth code stored"
+					+ twitter_access_token;
 		} else {
 			document.getElementById("result").innerHTML = sessionStorage.twitter_authentication_code
 					+ " is the previous code";
@@ -70,10 +197,9 @@ function store_codes(twitter_access_token, override) {
  * search engine
  */
 function OnRun() {
-
-
 	var check = check_authentication();
-	alert("Checked");
+	console.log("Checked Authentication");
+	/* alert("Checked"); */
 	if (check == false) {
 		return "Unsupported Browser";
 	}
@@ -86,18 +212,23 @@ function OnRun() {
 function check_authentication() {
 	if (typeof (Storage) !== "undefined") {
 		if (!sessionStorage.twitter_authentication_code) {
-			alert("REQUESTING1 done");
-			httpRequest('http://superfluous.imqs.co.za/omicron/authorise', store_codes,true);
+			console
+					.log("Requesting Omicron access code, New auth codes needed");
+			httpRequest('http://superfluous.imqs.co.za/omicron/authorise',
+					store_codes, true);
 
 			return true;
 		} else {
 			var access = sessionStorage.twitter_authentication_code;
 			if (validate_access_token() == false) {
-				alert("REQUESTING2");
-				httpRequest('http://superfluous.imqs.co.za/omicron/authorise', store_codes,true);
+				console
+						.log("Requesting Omicron access code, found invalid token");
+				httpRequest('http://superfluous.imqs.co.za/omicron/authorise',
+						store_codes, true);
 				return true;
 			} else {
-				document.getElementById("result").innerHTML = sessionStorage.twitter_authentication_code + " is the previous code";
+				document.getElementById("result").innerHTML = sessionStorage.twitter_authentication_code
+						+ " is the previous code";
 				return true;
 			}
 		}
