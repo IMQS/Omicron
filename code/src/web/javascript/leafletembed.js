1f24c1111b802a45d73349feb0d8f6190164d9f6
Javonne Martin <jovanne.martin@imqs.co.za>
2013-07-09 14:24:42 +0200

    Merging

--- code/src/web/javascript/leafletembed.js
+++ code/src/web/javascript/leafletembed.js
@@ -2,17 +2,14 @@
 function initmap() {
 	// set up the map
 	var map = L.map('mapHolder').setView([ 51.505, -0.09 ], 0);
-
 	L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		/* 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' */
 		maxZoom : 18
 	}).addTo(map);
-
 	var heatmap = L
 			.tileLayer(
 					'http://superfluous.imqs.co.za/omicron/request_handler?function=heat_map&platforms=twitter&tags=%23IMQS&location_type=radius&location=-33.964818_18.8372568_50000&directory=/{z}/{x}/{y}.png',
 					{
-
 						maxZoom : 18
 					});
 	L.control.layers(null, {
@@ -23,7 +20,6 @@ function initmap() {
 function get_input() {
 	var str = decodeURIComponent(window.location.search);
 	var objURL = {};
-	alert(str);
 	str.replace(new RegExp("([^?=&]+)(=([^&]*))?", "g"), function($0, $1, $2,
 			$3) {
 		objURL[$1] = $3;
@@ -33,11 +29,8 @@ function get_input() {
 function process_input(input) {
 	
 }
-
 function OnRun() {
 	var input = get_input();
 	process_input(input);
-	
-	
 	initmap();
 }
