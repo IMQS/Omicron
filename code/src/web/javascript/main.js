c0345de04deb0e87a9ad31bb829a32b95a8ec9a4
Javonne Martin <jovanne.martin@imqs.co.za>
2013-07-10 12:38:36 +0200

    fixed database url get request

--- code/src/web/javascript/main.js
+++ code/src/web/javascript/main.js
@@ -54,7 +54,7 @@ function database_request(callback,request_params,callback_params) // How can I 
 	}
 	console.log("Opening port");
 //	request.setRequestHeader("Content-length", 1);
-	request.open("GET", "http://superfluous.imqs.co.za/Omicron/request_search_id"+request_params);
+	request.open("GET", "http://superfluous.imqs.co.za/omicron/request_search_id"+request_params);
 	request.send();
 }
 
