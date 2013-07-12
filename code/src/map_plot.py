241c41fabb54cdbf808cdd5a5c11e86e2a5ee406
Leeward <michelle.arzul@gmail.com>
2013-07-12 11:04:07 +0200

    Streamlined, removed commented out code, removed unnecessary methods and updated in-code documentation.

--- code/src/map_plot.py
+++ code/src/map_plot.py
@@ -14,53 +14,21 @@ Created on 18 Jun 2013
   / /__/ / / / | | \ \ \ \ \ \___ | |\ \ | |_| | | | \ \| |
  /______/ /_/  |_|  \_\ \_\ \___/ |_| \_\ \___/  |_|  \___|
 
-A general-purpose plotter for scatter plots and heatmaps in any of the following coordinate systems:
+A general-purpose plotter for heatmaps in any of the following coordinate systems:
     > Geographic
     > Projected
     > Pixel
 
-CAVEAT: the random point generator makes tuples which matplotlib can interpret as (X, Y)
-    which makes sense on a Euclidean plane, but LAT/LON is in the reverse order!!!
+CAVEAT: the random point generator makes tuples which matplotlib can interpret as (X, Y) which
+    makes sense on a Euclidean plane or projected system, but LAT/LON is in the reverse order!!!
     Therefore, specify bounds as follows:
-        > [min_x, max_x, min_y, max_y] when working in (X, Y)
-        > [min_lat, max_lat, min_lon, max_lon] when working in LAT/LON
+        > [min_x, max_x, min_y, max_y] (l,r,b,t) when working in (X, Y)
+        > [min_lat, max_lat, min_lon, max_lon] (s,n,w,e) when working in LAT/LON
 
 CAVEAT: An exported tile is scaled between zero and the maximum of the tile. This will
     result in a patchy appearance unless some function is found to determine a scale that
     will work for the entire tileset.
 '''
-
-
-def plot(coords):
-    '''
-    Makes a simple scatter plot of given coordinates within a box that in X and Y spans the floor and ceiling integers of the minima and maxima respectively.
-    @param coords: The X, Y coordinates of points.
-    @type coords: List of 2-tuples of floats
-    @rtype: Void
-    '''
-    import matplotlib.pyplot as plt
-    x_coords = []
-    y_coords = []
-    for point in coords:
-        x_coords.append(point[0])
-        y_coords.append(point[1])
-        print "plotting: (" + str(point[0]) + ", " + str(point[1]) + ")"
-    plt.xlim(int(min(x_coords)), int(max(x_coords)) + 1)
-    plt.ylim(int(min(y_coords)), int(max(y_coords)) + 1)
-    plt.scatter(x_coords, y_coords,)
-    plt.show()
-
-def plot_random(num, bounds):
-    '''
-    Plots random points.
-    @param num: The number of points to generate.
-    @type num: Integer
-    @param bounds: The bounding coordinates of the points to generate, in the form [left, right, bottom, top].
-    @type bounds: List of 4 ints
-    @rtype: Void
-    '''
-    points = random_coords(num, bounds)
-    plot(points)
 
 def random_coords(num, bounds):
     '''
@@ -82,113 +50,6 @@ def random_coords(num, bounds):
         points.append((x, y))
     return points
 
-def heat_dist(point, x, y):
-    '''
-    Returns the Euclidean distance between a point and a pixel on an image.
-    @param point: The point's coordinates.
-    @type point: 2-tuple of floats.
-    @param x: The x-coordinate of the pixel.
-    @type x: Integer
-    @param y: The y-coordinate of the pixel.
-    @type y: Integer
-    @return: The Euclidean distance between the point and the pixel.
-    @rtype: Float
-    '''
-    from math import sqrt
-    return sqrt((point[0] - x-.5) ** 2 + (point[1] - y-.5) ** 2)
-    # the -.5 is to account for pixel center
-
-def gauss(var, x):
-    '''
-    Returns the y value at x for a normal distribution with variance var in the form f(x) = e^(-(x^2)/(2s^2)) where s is the spread. Note: the center of the curve is 0 since no mean is defined, and the maximum value is always 1.
-    @param var: The variance to use for this curve.
-    @type var: Float
-    @param x: The distance from the point.
-    @type x: Floatimport numpy as np
-        
-    @return: The value of the given curve at x.
-    @rtype: Float
-    '''
-    from math import e
-    return e ** (-(x * x) / (2 * var))
-
-def plot_gauss(var, lim):
-    '''
-    Plots a Gaussian distribution.
-    @param var: Variance of the curve.
-    @type var: Float
-    @param lim: Upper limit of plot (lower is 0).
-    @type lim: Float
-    @rtype: Void
-     coords = []
-    for i in xrange(lim):
-        for j in xrange(10):
-            num = i + j / 10.
-            print num, gauss(var, num)
-            coords.append((num, gauss(var, num)))
-    plot(coords)
-            
-    '''
-    
-    g = [i+j/10. for i in range(lim) for j in range(10) ]
-    coords = map(g, gauss(var, g))
-   
-    plot(coords)
-
-def heatmap(bounds, coords):
-    '''
-    Creates a heatmap within the specified bounds for the given coordinates. Variance is adjusted according to point density. If the area of the bounds is zero, None is returned.
-    @param bounds: The bounding coordinates of the points to generate, in the form [left, right, bottom, top].
-    @type bounds: List of 4 ints
-    @param coords: The X, Y coordinates of points.
-    @type coords: List of 2-tuples of floats.
-    @return: A pixel matrix heatmap for the given points.
-    @rtype: Rectangular numpy matrix of floats
-    '''
-    import numpy as np
-    from time import time
-    from math import sqrt
-    
-    rdim = bounds[1] - bounds[0]
-    cdim = bounds[3] - bounds[2]
-    
-    if cdim * rdim == 0:
-        print "AREA OF BOUNDS IS ZERO: NO HEATMAP RETURNED"
-        return None
-    
-    n = len(coords)
-    print n, "points provided."
-    
-    heatmap = np.array([[0.] * rdim] * cdim)
-    
-    print "For bounds " + str(bounds) + ", extent is " + str(rdim) + " by " + str(cdim) + " and area is " + str(rdim * cdim) + "."
-    
-    p_dens = float(n) / (rdim * cdim)
-    var = 1 / (8 * p_dens)
-    print "Point density of " + str(p_dens) + " gives a variance estimate of " + str(var) + "."
-    
-    three_stdev = int(3 * sqrt(var))
-    print "Distance cutoff for influence is " + str(three_stdev) + "."
-    
-    g = []
-    diag_dist = int(sqrt(cdim * cdim + rdim * rdim))
-    for i in xrange(three_stdev):
-        g.append(gauss(var, i))
-    while len(g) <= diag_dist:
-        g.append(0)
-    
-    print "Starting raster generation..."
-    s = time()
-    for i in xrange(cdim):
-        for j in xrange(rdim):
-            for point in coords:
-                heatmap[i][j] += g[int(heat_dist(point, j + bounds[0], i + bounds[2]))]
-                # gauss(var, heat_dist(point,j+bounds[0]+.5,i+bounds[2]+.5))
-    e = time()
-    print "Done."
-    print "Time: " + str(e - s) + " which per point is an average of: " + str((e - s) / n)
-    return heatmap
-
 def heatmap_tile(level=0, x=0, y=0, coords=[]):
     '''
     Creates a 256x256 heatmap tile for the specified zoom level and location for the given coordinates. Variance is fixed according to tile size.
@@ -203,7 +64,6 @@ def heatmap_tile(level=0, x=0, y=0, coords=[]):
     @return: A pixel matrix heatmap for the given points.
     @rtype: Rectangular numpy matrix of floats
     '''
-    
     
     import numpy as np
     from time import time
@@ -253,14 +113,6 @@ def heatmap_tile(level=0, x=0, y=0, coords=[]):
     print "\t\tTile points:"
     print "\t\t"+str(coords_tile)
     
-    #print "\tCreating Gauss curve..."
-    #g = []
-    #for i in xrange(57): # three_stdev = 57 # int(3 * sqrt(var)) hardcoded for efficiency
-    #    g.append(gauss(362, float(i)))
-    #while len(g) <= 543:  # int(sqrt((256*1.5)**2)*2) = 543 # diagonal length of one and a half tiles
-    #    g.append(0)
-    #print "\tDone."
-    
     print "\tStarting raster generation..."
     s = time()
     heatmap = np.array([[0.] * 512] * 512)
@@ -269,34 +121,27 @@ def heatmap_tile(level=0, x=0, y=0, coords=[]):
         heatmap[point_tile[1]+128,point_tile[0]+128] += 1000.
     heatmap = ndimage.gaussian_filter(heatmap, sigma=15)
     heatmap = heatmap[128:384,128:384]
-    #for i in xrange(256):
-    #    for j in xrange(256):
-    #        for point_tile in coords_tile:
-    #            heatmap[255-i][j] += g[int(heat_dist(point_tile, j, i))]
     
     e = time()
     print "\tDone."
     print "\tTime: " + str(e - s)
     print "\tAverage time per point: " + str((e - s) / n)
-    #heatmap = heatmap[::-1]
     print "Returning tile."
     return heatmap
 
-def show_heatmap(heatmap, bounds):
+def show_heatmap(heatmap):
     '''
-    Displays a heatmap within certain bounds using matplotlib.pyplot.
+    Displays a heatmap using matplotlib.pyplot.
     @param heatmap: A pixel matrix heatmap.
     @type heatmap: Rectangular numpy matrix of floats
-    @param bounds: The bounding coordinates of the points to generate, in the form [left, right, bottom, top].
-    @type bounds: List of 4 ints
     @rtype: Void2
     '''
     import matplotlib.pyplot as plt
     
     print "Displaying heat map..."
-    plt.xlim(bounds[0], bounds[1])
-    plt.ylim(bounds[2], bounds[3])
-    plt.imshow(heatmap, cmap='hot', origin='lower', extent=bounds)
+    plt.xlim(0, 256)
+    plt.ylim(0, 256)
+    plt.imshow(heatmap, cmap='hot', origin='lower', extent=[0,256,0,256])
     plt.show()
     print "Done."
 
@@ -365,7 +210,7 @@ def coords_to_geojson(coords):
     '''
     Converts a set of coordinate pairs to a GeoJSON object.
     @param coords: An array of coordinate pairs.
-    @type coords: Array or 2-tuples or length-2 arrays.
+    @type coords: Array of 2-tuples or length-2 arrays.
     @rtype: GeoJSON object
     '''
     import json
@@ -378,28 +223,20 @@ if __name__ == "__main__":
     
     from time import time
     s = time()
-    # XXX CAUTION! the random point generator makes tuples which matplotlib can interpret as (X, Y)
-    # which makes sense on a Euclidean plane, but LAT/LON is in the reverse order!!!
-    # Therefore, specify bounds as follows:
-    #    > [minx, maxx, miny, maxy] (l,r,b,t) when working in X, Y
-    #    > [miny, maxy, minx, maxx] (s,n,w,e) when working in LAT/LON
     
-    bounds_lim = [-85, 85, -180, 180]
-    #bounds_disp = [0, 256, 0, 256]
+    bounds_geo = [-85, 85, -180, 180]
     
-    ten_random = random_coords(10, bounds_lim)
+    ten_random = random_coords(10, bounds_geo)
     #smiley = [[25,45], [25,35], [35,25], [45,15], [55,15], [65,25], [75,35], [75,45], [35,75], [35,65], [65,65], [75,65]]
     #stellenbosch = [[-33.9200, 18.8600]]
     #center = [[0,0]]
     #square = [[45,45],[-45,45],[45,-45],[-45,-45]]
     #technopark = [[-33.964807, 18.8372767]]
     #madagascar = [[-20,47]]
-    #south_pole = [[-85,30]]
-    #north_pole = [[85,30]]
     
     #tile = heatmap_tile(level = 2, x = 1, y = 2, coords=stellenbosch*5)
-    #show_heatmap(tile, bounds_disp)
-    #show_3D_heatmap(heatmap)
+    #show_heatmap(tile)
+    #show_3D_heatmap(tile)
     #save_heatmap(tile, path="/home/marzul/test.png", colour=True)
     print coords_to_geojson(ten_random)
     e = time()
